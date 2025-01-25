2efb5d61643c8d07d7c1686dab5e19dfa0e0783b
Tahaali10 <syedtahali10@gmail.com>
2025-01-25 14:44:31 +0500

    ecommerce

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.jronline.app',
-  appName: 'Jhang Rang Online Store',
-  webDir: 'build'
-};
-
-export default config;
